b4a7af67a4d76cce3ef681c9f78484d89600fc1d
xSyriox <sir.quinti@stud.uniroma3.it>
2023-03-24 17:44:21 +0100

    +librerie test

---
+++ test/it/uniroma3/diadia/ComandoTest.java
@@ -0,0 +1,89 @@
+package it.uniroma3.diadia;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ComandoTest {
+
+	@Test
+	void testComando() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetParametro() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSconosciuto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
